3491544e08b2a8b9b1c32a097cf8932a49d1e18f
lastpapu <34819706+lastpapu@users.noreply.github.com>
2018-08-08 16:30:27 -0500

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,1 +1,8 @@
-alert("XOPA!!");
+var nombre=prompt("XOPA!!, como te llamas");
+
+var espacio = document.getElementById("espacio");
+function risa(){
+	espacio.innerHTML= "jajaja... "+nombre;
+	}
+
+risa();
